57a336cb94b9deeb85be2db6a5e49016cd9f2ea7
alf <alf@arenalogic.com>
2026-09-04 10:54:46 +0200

    Added osg::DrawMeshTasks implementaton.
    Partial cherry-pick: only the src/osg/GLExtensions.cpp hunk, which removes the
    mesh-shader OSG_NOTICE debug output. The osg::DrawMeshTasks class itself is not
    present in the vendored ACT tree, so its new files are deliberately left out.
    
    (cherry picked from commit 64404e9de88b0b0d8ee34cb4e6e6a02b41e8b8b0)

--- src/osg/GLExtensions.cpp
+++ src/osg/GLExtensions.cpp
@@ -1331,12 +1331,6 @@ GLExtensions::GLExtensions(unsigned int in_contextID):
     osg::setGLExtensionFuncPtr(glDrawMeshTasksIndirectNV, "glDrawMeshTasksIndirectNV", validContext);
     osg::setGLExtensionFuncPtr(glMultiDrawMeshTasksIndirectNV, "glMultiDrawMeshTasksIndirectNV", validContext);
     osg::setGLExtensionFuncPtr(glMultiDrawMeshTasksIndirectCountNV, "glMultiDrawMeshTasksIndirectCountNV", validContext);
-
-    OSG_NOTICE<<"isMeshShaderSupported = "<<isMeshShaderSupported<<std::endl;
-    OSG_NOTICE<<"glDrawMeshTasksNV = "<<glDrawMeshTasksNV<<std::endl;
-    OSG_NOTICE<<"glDrawMeshTasksIndirectNV = "<<glDrawMeshTasksIndirectNV<<std::endl;
-    OSG_NOTICE<<"glMultiDrawMeshTasksIndirectNV = "<<glMultiDrawMeshTasksIndirectNV<<std::endl;
-    OSG_NOTICE<<"glMultiDrawMeshTasksIndirectCountNV = "<<glMultiDrawMeshTasksIndirectCountNV<<std::endl;
 }
 
 GLExtensions::~GLExtensions()
